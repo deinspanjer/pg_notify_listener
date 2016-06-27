20f5b8c910ba107ee3d18ca626e66b3970eb0e85
Daniel Einspanjer <deinspanjer@gmail.com>
2016-06-26 21:01:20 -0400

    tweak retry to work right

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,9 +2,9 @@
 
 source /retry_func.sh
 
-retry 6 10 psql -h db -p 5432 -d postgres -U postgres -c 'select now();' || {
+retry 6 10 psql -q -h db -p 5432 -d postgres -U postgres -c 'select now();' || {
     echo "Could not connect to db container!"
     exit 1;
 }
 
-node pg_notify_listener.js
+node pg_notify_listener.js "$@"
--- retry_func.sh
+++ retry_func.sh
@@ -3,7 +3,7 @@
 # Retries a command on failure.
 # $1 - the max number of attempts
 # $2 - the time to wait before the next attempt
-#		(use 0 to use an incremental wait based on the current num attempts)
+#       (use 0 to use an incremental wait based on the current num attempts)
 # $3... - the command to run
 retry() {
     local -r -i max_attempts="$1"; shift
@@ -16,15 +16,15 @@ retry() {
         local -i incremental_delay=1;
     fi
         
-    until "${cmd[@]}"; do
-		retval=$?
+    until "${cmd[@]}" >/dev/null; do
+        retval=$?
         if (( attempt_num == max_attempts )); then
             echo "Attempt $attempt_num failed and there are no more attempts left!"
             exit $retval
         else
-			if (( incremental_delay == 1 )); then
-				(( wait_for += attempt_num ))
-			fi
+            if (( incremental_delay == 1 )); then
+                (( wait_for += attempt_num ))
+            fi
             echo "Attempt $attempt_num failed! Trying again in $wait_for seconds..."
             (( attempt_num++ ))
             sleep $wait_for
